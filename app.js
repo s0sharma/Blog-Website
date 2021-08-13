53044d6a767eb1dde58b707acef4af495d335e0b
Saurabh Sharma <saurabhkrsharma2000@gmail.com>
2021-08-13 15:46:08 +0530

    Added gitignore, procfile, port updated

--- app.js
+++ app.js
@@ -73,10 +73,13 @@ app.get("/posts/:postId", function(req, res){
      content: post.content
    });
   });
-
-
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started SUCCESSFULLY!");
 });
